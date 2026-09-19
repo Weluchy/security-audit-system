d70fa6a866395cae0dbd842db724a145d63f2c1d
Weluchy <andrushkevich.2001@mail.ru>
2026-09-19 19:43:14 +0300

    feat: add postgres connection and GET/POST routes

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
+	var err error
+	db, err = sql.Open("postgres", "host=localhost port=5432 user=myuser password=mypassword dbname=audit_logs sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if _, err := db.Exec(`create table if not exists events (
+	id generated always as identity primary key,
+	user_id int,
+	action varchar(255))`); err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/audit", handleAudit)
 	http.ListenAndServe(":8080", nil)
 }
@@ -17,19 +38,49 @@ type AuditRequest struct {
 }
 
 func handleAudit(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Не тот метод", http.StatusMethodNotAllowed)
-		return
-	}
+
 	var auditReq AuditRequest
-	if err := json.NewDecoder(r.Body).Decode(&auditReq); err != nil {
-		http.Error(w, "Неверный json", http.StatusBadRequest)
-		return
+
+	switch r.Method {
+	case http.MethodPost:
+		if err := json.NewDecoder(r.Body).Decode(&auditReq); err != nil {
+			http.Error(w, "Неверный json", http.StatusBadRequest)
+			return
+		}
+		if _, err := db.Exec(`insert into events (user_id, action) values ($1, $2)`, auditReq.UserID, auditReq.Action); err != nil {
+			http.Error(w, "Не получилось добавить данные", http.StatusInternalServerError)
+			return
+		}
+	case http.MethodGet:
+
+		rows, err := db.Query(`select user_id, action from events`)
+		if err != nil {
+			http.Error(w, "Не получилось просмотреть данные", http.StatusInternalServerError)
+			return
+		}
+
+		defer rows.Close()
+
+		var slice []AuditRequest
+		for rows.Next() {
+			var audit AuditRequest
+			if err := rows.Scan(&audit.UserID, &audit.Action); err != nil {
+				http.Error(w, "Ошибка rows", http.StatusInternalServerError)
+				return
+			}
+			slice = append(slice, audit)
+		}
+
+		if err = rows.Err(); err != nil {
+			http.Error(w, "Ошибка итерации", http.StatusInternalServerError)
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(slice); err != nil {
+			http.Error(w, "Ошибка кодировки json", http.StatusInternalServerError)
+			return
+		}
+
 	}
 
-	fmt.Printf("ID: %v, action: %v", auditReq.UserID, auditReq.Action)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte(`{"status":"Декордер отработал"}`))
 }
